17c2c7b673fd1577745053c6396beec6644351bc
Michael Wide <michael@bravikode.se>
2024-12-07 09:32:42 +0100

    Day07 part 2

--- src/Day07.kt
+++ src/Day07.kt
@@ -1,14 +1,14 @@
 fun main() {
 
-    val ops: List<(Long, Long) -> Long> = listOf(Long::plus, Long::times)
+    val ops: List<(Long, Long) -> Long> = listOf(Long::plus, Long::times, { a, b -> "$a$b".toLong() })
 
-    fun eval(result: Long, operands: List<Long>, acc: Long = 0): Boolean {
+    fun eval(result: Long, operands: List<Long>, operations: List<(Long, Long) -> Long>, acc: Long = 0): Boolean {
         val head = operands.first()
         val tail = operands.drop(1)
-        return ops.any {
+        return operations.any {
             when {
                 tail.isEmpty() -> it(acc, head) == result
-                else -> eval(result, tail, it(acc, head))
+                else -> eval(result, tail, operations, it(acc, head))
             }
         }
     }
@@ -18,11 +18,14 @@ fun main() {
         val calibrations = input.map { it.split(": ") }
             .map { cal -> cal[0].toLong() to cal[1].split(" ").map { it.toLong() } }
 
-        return calibrations.filter { (value, operands) -> eval(value, operands) }.sumOf { (value, _) -> value }
+        return calibrations.filter { (value, operands) -> eval(value, operands, ops.subList(0,2)) }.sumOf { (value, _) -> value }
     }
 
-    fun part2(input: List<String>): Int {
-        return 0
+    fun part2(input: List<String>): Long {
+        val calibrations = input.map { it.split(": ") }
+            .map { cal -> cal[0].toLong() to cal[1].split(" ").map { it.toLong() } }
+
+        return calibrations.filter { (value, operands) -> eval(value, operands, ops) }.sumOf { (value, _) -> value }
     }
 
     // Or read a large test input from the `src/Day01_test.txt` file:
@@ -33,5 +36,5 @@ fun main() {
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day07")
     part1(input).println()
-//    part2(input).println()
+    part2(input).println()
 }
